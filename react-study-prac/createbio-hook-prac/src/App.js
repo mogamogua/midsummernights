c0486496921e73af6e83256dd6762448d986d403
Jisu Kim <jso_osj@naver.com>
2021-10-26 02:47:16 +0900

    :sparkle: useEffect 예제코드
    인풋창 열고 닫을 때 안내 메시지 표시

--- react-study-prac/createbio-hook-prac/src/App.js
+++ react-study-prac/createbio-hook-prac/src/App.js
@@ -1,9 +1,11 @@
 import CreateBio from "./CreateBio";
+import CreateBioUseEffect from "./CreateBioUseEffect";
 
 function App() {
   return (
     <div className="App">
       <CreateBio />
+      <CreateBioUseEffect />
     </div>
   );
 }
