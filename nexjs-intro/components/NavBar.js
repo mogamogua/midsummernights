ae42a910ac57db2197f2eb9fc0bf33faa9fa65cb
JisuKim <jso_osj@naver.com>
2022-02-24 17:43:40 +0900

    [add]navbar

--- nexjs-intro/components/NavBar.js
+++ nexjs-intro/components/NavBar.js
@@ -1,27 +1,39 @@
 import Link from "next/link";
 import {useRouter} from "next/router";
-import styles from "./NavBar.module.css";
+// import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
       <Link href="/">
+        <a>Home</a>
         {/* <a style={{color: router.pathname === "/" ? "red" : "blue"}}>Home</a> */} 
         {/* <a className={router.pathname === "/" ? styles.active : ""}>Home</a> */}
-        <a className={`${styles.link} ${
+        {/* <a className={`${styles.link} ${
           router.pathname === "/" ? styles.active : ""
-        }`}>Home</a>
+        }`}>Home</a> */}
       </Link>
       <Link href="/about">
+        <a>About</a>
         {/* <a style={{color: router.pathname === "/about" ? "red" : "blue"}}>About</a> */}
         {/* <a className={router.pathname === "/about" ? styles.active : ""}>About</a> */}
-        <a
+        {/* <a
           className={[
             styles.link,
             router.pathname === "/about" ? styles.active : "",
-          ].join(" ")}>About</a>
+          ].join(" ")}>About</a> */}
       </Link>
-    </nav>
-  )
+      <style jsx>{`
+        nav {
+          background-color: tomato;
+        }
+        a {
+          text-decoration: none;
+        }
+        .active {
+          color: yellow;
+        }
+      `}</style>
+    </nav>  )
 }
