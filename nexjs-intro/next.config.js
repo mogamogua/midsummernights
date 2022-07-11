5e1ba96f2051e6e485d000cfb1b5239c04a38552
JisuKim <jso_osj@naver.com>
2022-07-11 20:21:46 +0900

    rewrite, redirect 연습 - api key 가리기

--- nexjs-intro/next.config.js
+++ nexjs-intro/next.config.js
@@ -1,3 +1,25 @@
+
+//개발자도구 네트워크 탭에서 요청 url에 apikey가 그대로 드러나기 때문에 감춰야한다.
+//(1) .env 파일 사용하기(환경변수) - 꼭 GITignore해주기!
+const API_KEY = process.env.API_KEY;
+
 module.exports = {
   reactStrictMode: true,
-}
+  async redirects() { //redirects는 바뀐 url을 그대로 보여줌. 
+    return [
+      {
+        source: "/old-blog/:path*", //:path*부분은 뒤에 무엇이와도 매칭된다.
+        destination: "/new-sexy-blog/:path*", //뒤의 path부분은 그대로 유지
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() { //rewrites는 바뀐 url을 감춘다.
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
--- nexjs-intro/pages/index.js
+++ nexjs-intro/pages/index.js
@@ -2,38 +2,51 @@ import Seo from "../components/Seo";
 import css from "styled-jsx/css";
 import { useEffect, useState } from "react";
 
-const API_KEY = "cf004ab885196276ebc4a6fddfb9cf18";
-
 export const divStyle = css`
   .back {
     background-color: lightgrey;
     padding: 2rem;
+  }
+  .container {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 20px;
+    gap: 20px;
+  }
+  .movie img {
+    max-width: 100%;
+    border-radius: 12px;
+    transition: transform 0.2s ease-in-out;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+  }
+  .movie:hover img {
+    transform: scale(1.05) translateY(-10px);
+  }
+  .movie h4 {
+    font-size: 18px;
+    text-align: center;
   }
 `;
 export default function Home() {
     const [movies, setMovies] = useState([]);
     useEffect(() => {
       (async () => {
-        const {results} = await (
-          await fetch(
-            `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-          )
-        ).json();
+        const {results} = await (await fetch(`/api/movies`)).json(); //rewrite하기 전 url로 요청한다
         setMovies(results)
       })();
     }, []);
   return (
-    <>
-    <Seo title="about" />
-    <div className="back">
-      <h1>HOME.</h1>
-      {!movies && <h4>Loading ...</h4>}
-      {movies?.map((movie) => (
-        <div key={movie.id}><h4>{movie.original_title}</h4></div>
-      ))}
+    <div className="container" >
+      <Seo title="Home" />
+        {!movies && <h4>Loading ...</h4>}
+        {movies?.map((movie) => (
+          <div className="movie" key={movie.id}>
+            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+            <h4>{movie.original_title}</h4>
+          </div>
+        ))}
+       <style jsx>{divStyle}</style>
     </div>
-    <style jsx>{divStyle}</style>
-    </>
   )
 }
 
