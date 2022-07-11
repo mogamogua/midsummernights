caf15391c44dfcbe4fc8b209368300564a03fb64
JisuKim <jso_osj@naver.com>
2022-07-11 16:01:00 +0900

    _app 작성

--- nexjs-intro/pages/_app.js
+++ nexjs-intro/pages/_app.js
@@ -1,9 +1,12 @@
 import NavBar from "../components/NavBar";
-import "../styles/globals.css";
+import "../styles/globals.css"; //custom App컴포넌트에서는 globalCss를 불러옰 ㅜ 있다.
 
+//global style 쓰고싶다면 _app.js를 직접 만들어 커스터마이징하기.
+
+//App 컴포넌트의 첫 prop은 component, 다른 하나는 pageProps이다.
 export default function MyApp({Component, pageProps}) {
   return (
-    <>
+    <> 
     <NavBar />
     <Component {...pageProps} />
     </>
--- nexjs-intro/pages/about.js
+++ nexjs-intro/pages/about.js
@@ -1,11 +1,14 @@
 import NavBar from "../components/NavBar";
+import { divStyle } from ".";
 
 export default function About() {
 
   return (
-    <div>
-      <NavBar />
+    <>
+    <div className="back">
       우리는
     </div>
+    <style jsx>{divStyle}</style>
+    </>
   )
 }
