aea507ece0b75ef151cd7b58e329617e25623ccd
JisuKim <jso_osj@naver.com>
2022-07-11 20:22:19 +0900

    :sparkle: ServerSideProps로 부분적 SSR하기

--- nexjs-intro/pages/index.js
+++ nexjs-intro/pages/index.js
@@ -27,19 +27,21 @@ export const divStyle = css`
     text-align: center;
   }
 `;
-export default function Home() {
+export default function Home({results}) {
     const [movies, setMovies] = useState([]);
-    useEffect(() => {
-      (async () => {
-        const {results} = await (await fetch(`/api/movies`)).json(); //rewrite하기 전 url로 요청한다
-        setMovies(results)
-      })();
-    }, []);
+    // useEffect(() => {
+    //   (async () => {
+    //     const {results} = await (await fetch(`/api/movies`)).json(); //rewrite하기 전 url로 요청한다
+    //     setMovies(results)
+    //   })();
+    // }, []);
+    //data를 받아오는 것은 server쪽에서 한 후 rendering한다.
+    //server-side rendering을 통해서 크롤러가 html을 크롤링할 때데이터 영역까지 모두ㅡ 볼 수 있도록.
+    //하지만 SSR은 데이터를 받아오기 전까진 화면에 아무것도 안 보이게 된다.
   return (
     <div className="container" >
       <Seo title="Home" />
-        {!movies && <h4>Loading ...</h4>}
-        {movies?.map((movie) => (
+        {results?.map((movie) => (
           <div className="movie" key={movie.id}>
             <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
             <h4>{movie.original_title}</h4>
@@ -50,3 +52,16 @@ export default function Home() {
   )
 }
 
+//getServerSideProps라는 함수 이름은 고정적. 바꿀 수 없다.
+//여기서 받아온 data들을 results라는 이름의 props로 page에 넘겨준다.
+export async function getServerSideProps() {
+  const {results} = await (
+    await fetch(`http://localhost:3000/api/movies`) 
+    //url은 client-side가 아니기 때문에 absolute URL만 가능하다. (/api/movies로 넣으면 X)
+  ).json();
+  return {
+    props: {
+      results,
+    }
+  }
+}
