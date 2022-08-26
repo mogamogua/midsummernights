19ba362e3b0b2f7a37d414bc5907a772d895727f
JisuKim <jso_osj@naver.com>
2022-08-27 00:26:07 +0900

    FEAT send state through Link Component

--- react-master/src/routes/Coin.tsx
+++ react-master/src/routes/Coin.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useState } from 'react'
+import { useLocation, useParams } from 'react-router-dom'
+import { Container, Header, Loader, Title } from './Coins'
+
+interface RouterParams {
+  coinId: string
+}
+
+interface RoutesState {
+  name: string
+}
 
 function Coin() {
+  const [loading, setLoading] = useState(true)
   const { coinId } = useParams()
+  const location = useLocation()
+  const state = location.state as RoutesState //Link에서 보내준 state받아오기
 
-  return <div>Coin: {coinId}</div>
+  return (
+    <Container>
+      <Header>
+        <Title>{state.name || 'loading...'}</Title>
+      </Header>
+      {loading ? <Loader>Loading...</Loader> : null}
+    </Container>
+  )
 }
 
 export default Coin
--- react-master/src/routes/Coins.tsx
+++ react-master/src/routes/Coins.tsx
@@ -1,61 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
-const coins = [
-  {
-    id: 'btc-bitcoin',
-    name: 'Bitcoin',
-    symbol: 'BTC',
-    rank: 1,
-    is_new: false,
-    is_active: true,
-    type: 'coin',
-  },
-  {
-    id: 'eth-ethereum',
-    name: 'Ethereum',
-    symbol: 'ETH',
-    rank: 2,
-    is_new: false,
-    is_active: true,
-    type: 'coin',
-  },
-  {
-    id: 'hex-hex',
-    name: 'HEX',
-    symbol: 'HEX',
-    rank: 3,
-    is_new: false,
-    is_active: true,
-    type: 'token',
-  },
-]
-
+interface CoinInterface {
+  id: string
+  name: string
+  symbol: string
+  rank: number
+  is_new: boolean
+  is_active: boolean
+  type: string
+}
 function Coins() {
+  const [coins, setCoins] = useState<CoinInterface[]>([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    //첫 렌더링시에만 실행되도록.
+    ;(async () => {
+      const response = await fetch('https://api.coinpaprika.com/v1/coins')
+      const json = await response.json()
+      setCoins(json.slice(0, 100))
+      setLoading(false)
+    })() // 즉시실행함수를 만들어서 오직 첫 렌더링에만 실행되도록한다.
+  }, [])
   return (
     <Container>
       <Header>
         <Title>코인</Title>
       </Header>
-      <CoinsList>
-        {coins.map((coin) => (
-          <Coin key={coin.id}>
-            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
-          </Coin>
-        ))}
-      </CoinsList>
+      {loading ? (
+        <Loader>Loading . . .</Loader>
+      ) : (
+        <CoinsList>
+          {coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link to={`/${coin.id}`} state={{ name: coin.name }}>
+                <Img
+                  src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                />
+                {coin.name} &rarr;
+              </Link>
+            </Coin>
+          ))}
+        </CoinsList>
+      )}
     </Container>
   )
 }
 
 export default Coins
 
-const Container = styled.div`
+export const Container = styled.div`
+  max-width: 480px;
+  margin: 0 auto;
   padding: 0px 20px;
 `
 
-const Header = styled.header`
+export const Header = styled.header`
   height: 15vh;
   display: flex;
   justify-content: center;
@@ -73,6 +75,8 @@ const Coin = styled.li`
     padding: 20px;
     transition: color 0.2s ease-in;
     display: block;
+    display: flex;
+    align-items: center;
   }
   &:hover {
     a {
@@ -81,7 +85,17 @@ const Coin = styled.li`
   }
 `
 
-const Title = styled.h1`
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
+`
+export const Loader = styled.span`
+  text-align: center;
+  display: block;
+`
+
+export const Title = styled.h1`
   font-size: 48px;
   color: ${(props) => props.theme.accentColor};
 `
