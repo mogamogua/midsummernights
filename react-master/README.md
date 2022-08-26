cd975e2a063243edc55741cf5108803477c8e246
JisuKim <jso_osj@naver.com>
2022-08-26 23:06:10 +0900

    INIT :tada: project setting - theme, router

---
+++ react-master/README.md
@@ -0,0 +1,2 @@
+## 노마드코더 react master class 강의
+
---
+++ react-master/src/Router.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Coin from './routes/Coin'
+import Coins from './routes/Coins'
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/:coinId" element={<Coin />} />
+        <Route path="/" element={<Coins />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Router
