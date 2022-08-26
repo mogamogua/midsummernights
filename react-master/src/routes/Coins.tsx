7d19eabb5bbac4bde18a6c0778d48025c31b7712
JisuKim <jso_osj@naver.com>
2022-08-26 23:17:13 +0900

    FEAT :sparkles: globalStyle 세팅

--- react-master/src/routes/Coins.tsx
+++ react-master/src/routes/Coins.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.accentColor};
+`
 
 function Coins() {
-  return (
-    <div>Coins</div>
-  )
+  return <div>Coins</div>
 }
 
 export default Coins
